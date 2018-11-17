7ee35d777e67a1559c64a046498c1d7479a9d694
Timothy Hobbs <timothyhobbs@seznam.cz>
2018-11-17 21:48:03 +0100

    Can now merge most fields in CellRuntime using rust
    Signed-off-by: Timothy Hobbs <timothyhobbs@seznam.cz>

--- src/gradesta-manager/gradestalib/src/merge.rs
+++ src/gradesta-manager/gradestalib/src/merge.rs
@@ -1,36 +1,74 @@
 use gradesta;
 use std::hash::Hash;
 use std::collections::HashMap;
+use std::borrow::Cow;
 
 /// # Merge input map into another HashMap
 /// ```
-/// #[macro_use] extern crate maplit;
-/// extern crate gradestalib;
-/// use gradestalib::merge::merge_map;
+/// # #[macro_use] extern crate maplit;
+/// # extern crate gradestalib;
+/// # use gradestalib::merge::merge_map;
 ///
 /// let input = hashmap!{
 ///   "foo" => "bar",
 ///   "bin" => "baz",
 /// };
+///
 /// let mut old = hashmap!{
 ///   "foo" => "lol",
 ///   "ma" =>  "pa",
 /// };
 ///
-/// merge_map(input, &mut old);
+/// merge_map(&input, &mut old);
 /// 
 /// let result = hashmap!{
 ///   "foo" => "bar",
 ///   "bin" => "baz",
 ///   "ma"  => "pa",
 /// };
+///
 /// assert_eq!(old, result);
 /// ```
-
-pub fn merge_map<A: Hash + Eq + Copy, B: Copy>(input: HashMap<A, B>, old: &mut HashMap<A, B>) {
+pub fn merge_map<A: Hash + Eq + Clone, B: Clone>(input: &HashMap<A, B>, old: &mut HashMap<A, B>) {
  for (key, value) in input.iter() {
   old.insert(key.clone(), value.clone());
  } 
+}
+
+macro_rules! set_if_some {
+ ($input:ident, $old:ident, $( $x:ident ),* ) => {
+   $(
+    if let Some($x) = $input.$x {
+     $old.$x = Some($x);
+    }
+   )*
+ }
+}
+
+macro_rules! merge_value_maps {
+ ($input:ident, $old:ident, $( $x:ident ),* ) => {
+   $(
+    merge_map(&$input.$x, &mut $old.$x);
+   )*
+ }
+}
+
+pub fn merge_cell_runtime(input: &gradesta::CellRuntime, old: &mut gradesta::CellRuntime) {
+ set_if_some![input, old,
+  update_count, 
+  click_count,
+  deleted];
+
+ if let Some(ref creation_id) = input.creation_id{
+  old.creation_id = Some(Cow::from(creation_id.to_string()));
+ }
+
+ merge_value_maps![input, old,
+  cell_runtime_modes,
+  cell_modes,
+  link_modes,
+  for_link_modes,
+  back_link_modes];
 }
 
 #[cfg(test)]
